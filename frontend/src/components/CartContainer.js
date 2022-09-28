e0c633c28a2110c6749f5e831c92de05669ef40e
BaLin <alexanderlinss@web.de>
2022-09-28 07:17:32 +0200

    able to fetch data

--- frontend/src/components/CartContainer.js
+++ frontend/src/components/CartContainer.js
@@ -3,13 +3,38 @@ import { useState, useEffect } from "react";
 import Cart from "./Cart";
 
 const CartContainer = () => {
-  const [state, setstate] = useState(0);
+  const [data, setData] = useState(0);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    console.log("erstes rendering");
-    return () => {
-      // cleanup
-    };
+    // fetch("http://localhost:5000/api/products")
+    fetch("/api/products")
+      .then((response) => {
+        if (response) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => setData(data))
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      })
+      .finally(setLoading(false));
   }, []);
+
+  if (loading) {
+    return <>loading...</>;
+  }
+
+  if (error) {
+    return <>error..</>;
+  }
+
+  if (data) {
+    console.log("data: ", JSON.stringify(data));
+  }
 
   return (
     <>
